0714451c8bdcbb5f955829da361c111b359529bd
gajeray5 <153584555+gajeray5@users.noreply.github.com>
2025-03-25 11:12:46 +0530

    Deploy Nuxt to GitHub Pages

--- _nuxt/Bf9I_wwV.js
+++
@@ -1,1 +0,0 @@
-import{p as s}from"./BmTBFpJA.js";const p=s("/Images/background/round.png");export{p as _};
--- _nuxt/UGEg2OYM.js
+++
@@ -1,1 +0,0 @@
-import{P as o,k as s}from"./BmTBFpJA.js";const n={__name:"index",setup(t){const e=o();return s(()=>{e.push("/home")}),()=>{}}};export{n as default};
