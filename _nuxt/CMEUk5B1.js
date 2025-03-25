d9e4e00c4011423c3c746d6804104c3b48c9833f
gajeray5 <153584555+gajeray5@users.noreply.github.com>
2025-03-25 10:45:10 +0530

    update shreeom code

---
+++ _nuxt/CMEUk5B1.js
@@ -0,0 +1,1 @@
+import{O as o,n as s}from"./X0Se_K80.js";const r={__name:"index",setup(t){const e=o();return s(()=>{e.push("/home")}),()=>{}}};export{r as default};
---
+++ _nuxt/DTf4ehVt.js
@@ -0,0 +1,1 @@
+import{p as s}from"./X0Se_K80.js";const p=s("/Images/background/round.png");export{p as _};
