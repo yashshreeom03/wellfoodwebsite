1700e77df78451d0496dc76f9592b2a33c2fd323
gajeray5 <153584555+gajeray5@users.noreply.github.com>
2025-04-04 14:43:53 +0530

    update shreeom code

--- _nuxt/8VHN7fxp.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./DlAUqK2U.js";import{c as e,o as c}from"./Boju9AsU.js";const n={};function r(t,a){return c(),e("p",null,"welcome to Company")}const _=o(n,[["render",r]]);export{_ as default};
--- _nuxt/C3gtiffw.js
+++ _nuxt/C3gtiffw.js
@@ -1,4 +1,4 @@
-import{j as on,k as W,A as sn,y as ln}from"./Boju9AsU.js";/*!
+import{j as on,k as W,z as sn,x as ln}from"./HefIpC4c.js";/*!
  * Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com
  * License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
  * Copyright 2024 Fonticons, Inc.
