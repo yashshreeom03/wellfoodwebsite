5101bfec78a56ed6268f23eff6118809c736741b
gajeray5 <153584555+gajeray5@users.noreply.github.com>
2025-04-09 11:39:24 +0530

    update shreeom code

---
+++ _nuxt/DcFN-Zrw.js
@@ -0,0 +1,41 @@
+const e=[{title:"UI/UX Designer",subtitle:"On site, Full Time, 0 - 1 Year of experience",description:`
+      <section class="py-[100px]">
+        <div class="container">
+            <div class="flex max-lg:flex-col gap-6 justify-between">
+                <div>
+                    <h2 class="text-2xl font-medium mb-8">Roles and Responsibilities</h2>
+                    <p class="text-lg leading-9">We are looking freshers and experience ui ux designer, if you have good knowledge in ui ux design side and skill. apply now to join our growing team!.</p>
+                     <strong class="text-lg leading-9">Requirements:</strong>
+                     <ul>
+                        <li> &#8594; Strong proficiency in UX/UI design principles and tools.</li>
+                        <li> &#8594; Excellent communication and collaboration skills.</li>
+                        <li> &#8594; Ability to work independently and as part of a team..</li>
+                        <li> &#8594; Proficient in Figma, Sketch, Adobe XD, or similar tools.</li>
+                        <li> &#8594; Strong user-centered design, usability, and accessibility knowledge.</li>
+                        <li> &#8594; Create wireframes, prototypes, and high-fidelity designs</li>
+                      </ul>
+                </div>
+                <div>
+                    <div class="lg:min-w-[426px] w-full border border-[#EDEDED] rounded-xl p-[30px]">
+                    <div class="mb-5">
+                        <h4 class="text-lg font-semibold">Position :
+                        <span class="text-lg font-medium">UI/UX Designer</span></h4>
+                    </div>
+                    <div class="mb-5">
+                        <h4 class="text-lg font-semibold">Experience Level :
+                        <span class="text-lg font-medium"> 0 - 1 years</span></h4>
+                    </div>
+                    <div class="mb-5">
+                        <h4 class="text-lg font-semibold">Location :
+                        <span class="text-lg font-medium">Surat(Onsite)</span></h4>
+                    </div>
+                    <div class="mb-5">
+                        <h4 class="font-semibold text-lg">Type :
+                        <span class="text-lg font-medium">Full Time</span></h4>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+      </section>
+    `,applyLink:"https://shreeomsoft.com/contact",category:"Designer"}];export{e as j};
